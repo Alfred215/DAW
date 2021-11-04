2fa5f93416b562d9451b8d1fd3f3d24e77f94299
Alfred215 <alfredoarpalrgmail.com>
2021-11-04 20:24:29 +0100

    son las 20:24 del 4/11
    Signed-off-by: Alfred215 <alfredoarpalrgmail.com>

--- DAWCliente/Unidad_2/U2_T1_Sintaxis_Basica_Alfredo_Garcia_Arpal/Ejercicio3/horario.js
+++ DAWCliente/Unidad_2/U2_T1_Sintaxis_Basica_Alfredo_Garcia_Arpal/Ejercicio3/horario.js
@@ -1,12 +1,26 @@
 var deportes = ["Aerobic","Ciclismo","Yoga","KickBoxing","Crossfit","Karate","Sgrima","Sumo","Aquagym","Zumba","Body combat","Body pump","Spinning","Xcore"];
 
+function insertar(){
+    
+}
+/*
 for(var a =1;a<15;a++){
     for(var i = 0; i<6;i++){
-        var elemento = document.createElement("td"); 
-        var texto = document.createTextNode(deportes[a])
-        elemento.appendChild(texto);  
+        
     }
-     var lista = document.getElementById(a);
-      lista.appendChild(elemento);
+    
 }
+*/
+var elemento = document.createElement("li");
+var texto = document.createTextNode("Elemento de prueba");
+elemento.appendChild(texto);
 
+var lista = document.getElementById("lista");
+lista.appendChild(elemento);
+var nuevoElemento = "<li> Texto prueba</li>";
+lista.innerHTML = lista.innerHTML + nuevoElemento;
+var elemento = document.createElement("li"); 
+var texto = document.createTextNode("adios")
+elemento.appendChild(texto);   
+var lista = document.getElementById("lista");
+lista.appendChild(elemento);
