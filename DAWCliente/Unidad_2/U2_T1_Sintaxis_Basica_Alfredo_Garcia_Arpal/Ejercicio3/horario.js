95a1d35a1d03c30aabebc7d2bbbf7fc6340e463c
Alfred215 <alfredoarpalrgmail.com>
2021-11-04 20:17:49 +0100

    son las 20:17 del 4/11
    Signed-off-by: Alfred215 <alfredoarpalrgmail.com>

--- DAWCliente/Unidad_2/U2_T1_Sintaxis_Basica_Alfredo_Garcia_Arpal/Ejercicio3/horario.js
+++ DAWCliente/Unidad_2/U2_T1_Sintaxis_Basica_Alfredo_Garcia_Arpal/Ejercicio3/horario.js
@@ -1,12 +1,12 @@
 var deportes = ["Aerobic","Ciclismo","Yoga","KickBoxing","Crossfit","Karate","Sgrima","Sumo","Aquagym","Zumba","Body combat","Body pump","Spinning","Xcore"];
 
 for(var a =1;a<15;a++){
-    var elemento= document.createElement("th");
-    var lista = document.getElementById(a);
     for(var i = 0; i<6;i++){
-        var texto = document.write(deportes[a]);
-        elemento.appendChild(texto);
-        lista.appendChild(elemento);
+        var elemento = document.createElement("td"); 
+        var texto = document.createTextNode(deportes[a])
+        elemento.appendChild(texto);  
     }
+     var lista = document.getElementById(a);
+      lista.appendChild(elemento);
 }
 
