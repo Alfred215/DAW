b5bd627b1fb74e3520f63e34bb40946b551bf9a2
Alfred215 <alfredoarpalrgmail.com>
2021-11-05 15:48:36 +0100

    son las 15:48 del 5/11
    Signed-off-by: Alfred215 <alfredoarpalrgmail.com>

--- DAWCliente/Unidad_2/U2_T1_Sintaxis_Basica_Alfredo_Garcia_Arpal/Ejercicio3/horario.js
+++ DAWCliente/Unidad_2/U2_T1_Sintaxis_Basica_Alfredo_Garcia_Arpal/Ejercicio3/horario.js
@@ -1,21 +1,25 @@
-var deportes = ["Aerobic","Ciclismo","Yoga","KickBoxing","Crossfit","Karate","Sgrima","Sumo","Aquagym","Zumba","Body combat","Body pump","Spinning","Xcore"];
-for(var a=1;a<15;a++){
-    for(var i = 0; i<6;i++){
-        var elemento = document.createElement("td");
-        var texto = document.createTextNode(deportes[a]) 
-        elemento.appendChild(texto);   
-        var lista = document.getElementById(a);
-        lista.appendChild(elemento); 
-    }   
-}  
+function crear(){
+    var deportes = ["Aerobic","Ciclismo","Yoga","KickBoxing","Crossfit","Karate","Sgrima","Sumo","Aquagym","Zumba","Body combat","Body pump","Spinning","Xcore"];
+    for(var a=1;a<15;a++){
+        for(var i = 0; i<6;i++){
+            var elemento = document.createElement("td");
+            var texto = document.createTextNode(deportes[a]) 
+            elemento.appendChild(texto);   
+            var lista = document.getElementById(a);
+            lista.appendChild(elemento); 
+        }   
+    }    
 
-for(var b=15;b<25;b++){
     var fisios = ["Alfredo","Alfredo","Maria","Maria","Sonia","Sonia","Alberto","Alberto","Sergio","Sergio"];
-    for(var j = 0; j<5;j++){
-        var elemento = document.createElement("td");
-        var texto = document.createTextNode(fisios[(b-15)]) 
-        elemento.appendChild(texto);   
-        var lista = document.getElementById(b);
-        lista.appendChild(elemento); 
-    }   
-}  
+    for(var b=15;b<25;b++){
+        for(var j = 0; j<5;j++){
+            var elemento = document.createElement("td");
+            var texto = document.createTextNode(fisios[(b-15)]) 
+            elemento.appendChild(texto);   
+            var lista = document.getElementById(b);
+            lista.appendChild(elemento); 
+        }   
+    }      
+}
+
+
