a42e0f7241f22a70d2c4d598a779d6cc4a68e59e
Alfred215 <alfredoarpalrgmail.com>
2021-11-05 16:05:48 +0100

    son las 16:05 del 5/11
    Signed-off-by: Alfred215 <alfredoarpalrgmail.com>

--- DAWCliente/Unidad_2/U2_T1_Sintaxis_Basica_Alfredo_Garcia_Arpal/Ejercicio3/horario.js
+++ DAWCliente/Unidad_2/U2_T1_Sintaxis_Basica_Alfredo_Garcia_Arpal/Ejercicio3/horario.js
@@ -1,22 +1,22 @@
 function crear(){
-    var deportes = ["Aerobic","Ciclismo","Yoga","KickBoxing","Crossfit","Karate","Sgrima","Sumo","Aquagym","Zumba","Body combat","Body pump","Spinning","Xcore"];
-    for(var a=1;a<15;a++){
+    var deportes = ["Aerobic","Ciclismo","Yoga","KickBoxing","Crossfit","Karate","Sgrima"];
+    for(var a=1;a<8;a++){
         for(var i = 0; i<6;i++){
             var elemento = document.createElement("td");
-            var texto = document.createTextNode(deportes[a]) 
+            var texto = document.createTextNode(deportes[a-1]) 
             elemento.appendChild(texto);   
-            var lista = document.getElementById(a);
+            var lista = document.getElementById("deporte"+a);
             lista.appendChild(elemento); 
         }   
     }    
 
     var fisios = ["Alfredo","Alfredo","Maria","Maria","Sonia","Sonia","Alberto","Alberto","Sergio","Sergio"];
-    for(var b=15;b<25;b++){
+    for(var b=1;b<11;b++){
         for(var j = 0; j<5;j++){
             var elemento = document.createElement("td");
-            var texto = document.createTextNode(fisios[(b-15)]) 
+            var texto = document.createTextNode(fisios[(b-1)]) 
             elemento.appendChild(texto);   
-            var lista = document.getElementById(b);
+            var lista = document.getElementById("fisio"+b);
             lista.appendChild(elemento); 
         }   
     }      
