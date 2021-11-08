09324ea65bdfb4c092f4b70e071e67eab4888bae
Alfred215 <alfredoarpal@gmail.com>
2021-11-08 13:36:50 +0100

    son las 13:36 del 8/11

--- DAWCliente/Unidad_2/U2_T1_Sintaxis_Basica_Alfredo_Garcia_Arpal/Ejercicio4/horario.js
+++ DAWCliente/Unidad_2/U2_T1_Sintaxis_Basica_Alfredo_Garcia_Arpal/Ejercicio4/horario.js
@@ -23,8 +23,48 @@ function crear(){
     }      
 }
 
-function resaltarActividad(actividad){
+function resaltarActividad(){
+    var  seleccionar  =  [];
+    var  actividad  =  document.getElementsByName("deportes");
+    var  table1  =  document.getElementById("table1");
+    var  celdas  =  table1.getElementsByTagName('td');
 
+    for(var i=0;i<actividad.length;i++) {
+        if( actividad[i].checked==true) {
+            seleccionar.push(actividad[i].value)
+        }
+    }
+
+    for(var i=0;i<=celdas.length-1;i++) {
+        for(var j=0;j<=seleccionar.length-1;j++) {
+            if ( celdas[i].innerHTML==seleccionar[j]) {
+                celdas[i].style.backgroundColor ="green";
+            }
+        }
+    }
+}
+
+function resaltarFisio(){
+    var seleccion="";
+    var fisio = document.getElementsByName("fisio");
+    var table2 = document.getElementById("table2");
+    var celdas = table2.getElementsByTagName('td');
+
+    for(var i=0;i<fisio.length;i++){
+        if(fisio[i].checked==true){
+            seleccion= fisio[i].value;
+        }
+    }
+
+    for(var i=0;i<5;i++){
+        for(var j=0;j<celdas.length-1;j++){
+            if(celdas[j].innerHTML==seleccion){
+                celdas[j].style.backgroundColor="green";
+            }else{
+                celdas[j].style.backgroundColor="white";
+            }
+        }
+    }
 }
 
 
